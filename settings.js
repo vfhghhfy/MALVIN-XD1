99268caff03272d03e4dcfaf77f12bf2f29c139a
Malvin King <malvink003@gmail.com>
2025-03-07 14:22:55 +0200

    Add files via upload

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~tw4QADrb#3vIeHvoICLX9uIJR1B52ygD5VfJSByWSrGcpBNQAn-c",
 // add your Session Id ,session id starts with MALVIN-XD~
 
 PREFIX: process.env.PREFIX || ".",
