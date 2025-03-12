ae6d69d0db7ef41362b7f7a812513467a8959e19
Malvin King <malvink003@gmail.com>
2025-03-12 14:45:15 +0200

    Update check-update.js

--- malvin/plugins/check-update.js
+++ malvin/plugins/check-update.js
@@ -3,7 +3,7 @@ const os = require('os');
 const fs = require('fs');
 const path = require('path');
 const { cmd, commands } = require('../command');
-const { runtime } = require('../lib/functions');
+const { runtime } = require('../../lib/functions');
 
 cmd({
   pattern: 'version',
